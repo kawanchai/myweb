cb0a1e8f47cb2121006ab787bd1d43559c902ff6
Kawanchai Thongkam <peachandpam@gmail.com>
2015-08-11 13:38:39 +0700

    Revert "Revert "Revert "test upload"""
    This reverts commit 72eebda1f00700883ae298fcac53d18f6028650e.

--- src/home.js
+++
@@ -1,9 +0,0 @@
-import {bindable} from 'aurelia-framework';
-
-export class Home {
-  @bindable router = null;
-
-  $('.carousel').carousel({
-  interval: 2000
-  })
-}
--- src/menu.js
+++
@@ -1,5 +0,0 @@
-import {bindable} from 'aurelia-framework';
-
-export class Menu {
-  @bindable router = null;
-}
